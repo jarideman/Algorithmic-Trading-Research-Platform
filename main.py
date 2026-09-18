b024704c13eac57d3c06a60218a8344c866b450f
Jari van den Boogert <jarivandenboogert@gmail.com>
2026-09-18 20:52:57 +0200

    ATRP-5 data-analysis

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import MetaTrader5 as mt5
 import pandas as pd
 import config
 
+from connection.mt5_connection import init_mt5, deinit_mt5
 from validation.rates_validation import validate_rates
 from validation.gaps import detect_gaps
 from storage.csv_storage import save_rates
@@ -45,23 +46,6 @@ def main():
 
     finally:
         deinit_mt5()
-
-def init_mt5():
-    if not mt5.initialize():
-        print('MT5 initialization failed')
-        return False
-
-    if not mt5.symbol_select(SYMBOL, True):
-        print('Symbol not found')
-        deinit_mt5()
-        return False
-
-    print('MT5 initialization successful')
-    return True
-
-def deinit_mt5():
-    mt5.shutdown()
-    return
 
 def get_rates():
     rates = mt5.copy_rates_from_pos(
